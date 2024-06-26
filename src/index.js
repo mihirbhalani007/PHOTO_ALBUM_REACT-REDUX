f5d59d3e0bd5a2e8f35ae250045fb4bda2dcbf23
Mihir Patel <mihirbhalani18@gmail.com>
2024-06-26 18:48:17 +0530

    set up redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 import "./Index.css";
+import { Provider } from "react-redux";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
