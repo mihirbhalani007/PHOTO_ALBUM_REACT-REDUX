7cc77dd0289cbe45a7f249c2b38bc972679f0258
Mihir Patel <mihirbhalani18@gmail.com>
2024-06-27 11:54:32 +0530

    add 'addUser' feature to this app, when addUser btton is clicked a rendomely generated userName added to the userLIst

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 function UsersList() {
   const dispatch = useDispatch();
@@ -13,15 +14,37 @@ function UsersList() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) {
-    return <Skeleton times={15} className="h-10 w-full" />;
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
     return <div>Error fetching data.....</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
