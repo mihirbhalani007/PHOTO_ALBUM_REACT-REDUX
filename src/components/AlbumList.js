8d28807181843d1bd13e238956e9459026ece73a
Mihir Patel <mihirbhalani18@gmail.com>
2024-07-01 20:53:46 +0530

    Render Fetched data from Albums data using RTK Query

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,33 @@
-import { useFetchAlbumQuery } from "../store";
+import { useFetchAlbumQuery } from "../store/Apis/albumsApi";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
 
 function AlbumList({ user }) {
   const { data, error, isLoading } = useFetchAlbumQuery(user);
-  console.log(data, error, isLoading);
-  return <div>ALbums for {user.name}</div>;
-}
 
+  let content;
+
+  if (isLoading) {
+    content = <Skeleton times={5} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos of the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>ALbums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
+}
 export default AlbumList;
