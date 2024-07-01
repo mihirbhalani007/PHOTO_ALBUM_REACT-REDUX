190321c29734b4ed007e0345ba333c73e45277e1
Mihir Patel <mihirbhalani18@gmail.com>
2024-07-01 20:25:37 +0530

    Add 'albumApi' to this app and create Api for albums fetching with RTK Query

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,4 +1,8 @@
+import { useFetchAlbumQuery } from "../store";
+
 function AlbumList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumQuery(user);
+  console.log(data, error, isLoading);
   return <div>ALbums for {user.name}</div>;
 }
 
