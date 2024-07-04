dd1947c6f19ae10a9e6fd6c268c06b9eb0b08f3e
Mihir Patel <mihirbhalani18@gmail.com>
2024-07-04 16:03:32 +0530

    Render radomly generated photo and add functionalyity to remove photo

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,13 +1,26 @@
-import { useAddAlbumMutation, useFetchPhotosQuery } from "../store";
+import { useAddPhotoMutation, useFetchPhotosQuery } from "../store";
 import Button from "./Button";
+import Skeleton from "./Skeleton";
+import PhotoListItem from "./PhotoListItem";
 
 function PhotoList({ album }) {
-  useFetchPhotosQuery(album);
-  const [addPhoto, addPhotoResults] = useAddAlbumMutation();
+  const { data, isFetching, error } = useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
 
   const handleAddPhoto = () => {
     addPhoto(album);
   };
+  let content;
+
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-10" times={3} />;
+  } else if (error) {
+    content = <div>Error loading photos.</div>;
+  } else {
+    content = data.map((photo) => {
+      return <PhotoListItem key={photo.id} photo={photo} />;
+    });
+  }
 
   return (
     <div>
@@ -16,6 +29,9 @@ function PhotoList({ album }) {
         <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
           + Add Photo
         </Button>
+      </div>
+      <div className="mx-8 flex flex-row flex-wrap justify-center">
+        {content}
       </div>
     </div>
   );
